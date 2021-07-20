e4f3698021a88a6dcf15320d97e33e20393ddf5b
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-07-20 20:15:31 +0530

    feat: update batch container

--- src/components/fitnessProfile/BatchContainer.tsx
+++ src/components/fitnessProfile/BatchContainer.tsx
@@ -1,36 +1,69 @@
 import React from "react";
 import {Text, View} from "react-native";
 import {scale, ScaledSheet} from "react-native-size-matters";
-import {CONTENT, HEAD_TEXT, OFF_WHITE, PRIMARY} from "../../assets/constants/colors";
+import {
+  CONTENT,
+  LIGHT_GREY,
+  OFF_WHITE,
+  PRIMARY,
+} from "../../assets/constants/colors";
 import {APP_MARGIN_HORIZONTAL} from "../../assets/constants/styles";
+import {formatTimeSlot} from "../../utils/dateTimeMethods";
+import {getPlanDays} from "../../utils/plansMethods";
 import AppButton from "../common/AppButton";
 
-const BatchContainer = () => {
+interface IProps {
+  plans: Array<{
+    id: string;
+    type: number;
+    batch: number;
+    timeSlotTo: string;
+    timeSlotFrom: string;
+    price: string;
+    days: Array<number>;
+  }>;
+}
+
+const BatchContainer = ({plans}: IProps) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.headText}>Batch-1</Text>
-      <View style={styles.mainRowContainer}>
-        <View style={styles.detailsContainer}>
-          <Text style={styles.daysText}>
-            Mon, Tue, Wed, Thrus, Fri, Sat, Sun
-          </Text>
-          <Text style={styles.timeSlotText}>07:00 AM - 08:00AM</Text>
+      {plans.map((plan) => (
+        <View key={plan.id}>
+          <Text style={styles.headText}>Batch-{plan.batch}</Text>
+          <View style={styles.mainRowContainer}>
+            <View style={styles.detailsContainer}>
+              <Text style={styles.daysText}>
+                {getPlanDays(plan.days).join(", ")}
+              </Text>
+
+              <Text style={styles.timeSlotText}>
+                {formatTimeSlot(plan.timeSlotFrom)} -{" "}
+                {formatTimeSlot(plan.timeSlotTo)}
+              </Text>
+            </View>
+
+            <View style={styles.bookingContainer}>
+              <Text style={styles.priceText}>₹ {plan.price}</Text>
+
+              <AppButton
+                text="Book"
+                textStyle={{
+                  color: PRIMARY,
+                  fontSize: scale(14),
+                  fontWeight: "500",
+                }}
+                containerStyle={{
+                  borderColor: PRIMARY,
+                  borderWidth: scale(1),
+                  paddingHorizontal: scale(18),
+                  paddingVertical: scale(2),
+                  borderRadius: scale(8),
+                }}
+              />
+            </View>
+          </View>
         </View>
-        <View style={styles.bookingContainer}>
-          <Text style={styles.priceText}>₹ 500</Text>
-          <AppButton
-            text="Book"
-            textStyle={{color: PRIMARY, fontSize: scale(14), fontWeight: "500"}}
-            containerStyle={{
-              borderColor: PRIMARY,
-              borderWidth: scale(1),
-              paddingHorizontal: scale(10),
-              paddingVertical: scale(8),
-              borderRadius: scale(8)
-            }}
-          />
-        </View>
-      </View>
+      ))}
     </View>
   );
 };
@@ -46,10 +79,10 @@ const styles = ScaledSheet.create({
   mainRowContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: 'center'
+    alignItems: "center",
   },
   headText: {
-    color: HEAD_TEXT,
+    color: CONTENT,
     fontSize: "15@s",
     fontWeight: "bold",
     marginTop: "20@s",
@@ -58,23 +91,24 @@ const styles = ScaledSheet.create({
     marginTop: "15@s",
   },
   daysText: {
-    maxWidth: "200@s",
+    maxWidth: "140@s",
     fontSize: "13@s",
-    color: HEAD_TEXT,
-    marginBottom: "10@s"
+    color: LIGHT_GREY,
+    marginBottom: "10@s",
   },
   timeSlotText: {
     fontSize: "12@s",
-    color: CONTENT
+    color: CONTENT,
   },
   bookingContainer: {
     marginTop: "15@s",
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   priceText: {
-    fontSize: "13@s",
-    color: HEAD_TEXT,
-    marginBottom: "10@s"
+    fontSize: "12@s",
+    color: LIGHT_GREY,
+    fontWeight: "700",
+    marginBottom: "10@s",
   },
 });
