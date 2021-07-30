1842633f4db1de1f0ddd787b35e747f9ea5cc1f5
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-07-31 04:55:02 +0530

    refactor: error handling

--- src/helpers/reviewMethods.ts
+++ src/helpers/reviewMethods.ts
@@ -3,6 +3,7 @@ import {
   CREATE_REVIEWS,
   UPDATE_USER_BOOKING_REVIEW_STATUS,
 } from "../queries/mutation";
+import {sentryError} from "../utils/sentrySetup";
 
 export const createReview = async (
   fitnessServiceId: string,
@@ -10,20 +11,28 @@ export const createReview = async (
   review: string,
   userEmail: string,
 ) => {
-  const res = await API.graphql(
-    graphqlOperation(CREATE_REVIEWS, {
-      fitnessServiceId,
-      ratings,
-      review,
-      userEmail,
-    }),
-  );
+  try {
+    await API.graphql(
+      graphqlOperation(CREATE_REVIEWS, {
+        fitnessServiceId,
+        ratings,
+        review,
+        userEmail,
+      }),
+    );
+  } catch (error) {
+    sentryError(error);
+  }
 };
 
 export const updateReview = async (id: string) => {
-  const res = await API.graphql(
-    graphqlOperation(UPDATE_USER_BOOKING_REVIEW_STATUS, {
-      id,
-    }),
-  );
+  try {
+    await API.graphql(
+      graphqlOperation(UPDATE_USER_BOOKING_REVIEW_STATUS, {
+        id,
+      }),
+    );
+  } catch (error) {
+    sentryError(error);
+  }
 };
