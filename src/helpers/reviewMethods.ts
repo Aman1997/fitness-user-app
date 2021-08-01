3ba1222728ac869a0784636f75fbbcb7a109c0ac
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-08-01 10:44:21 +0530

    feat: update new review methods

--- src/helpers/reviewMethods.ts
+++ src/helpers/reviewMethods.ts
@@ -2,6 +2,7 @@ import {API, graphqlOperation} from "aws-amplify";
 import {
   CREATE_REVIEWS,
   UPDATE_USER_BOOKING_REVIEW_STATUS,
+  UPDATE_USER_MEMBERSHIP_REVIEW_STATUS,
 } from "../queries/mutation";
 import {sentryError} from "../utils/sentrySetup";
 
@@ -25,13 +26,28 @@ export const createReview = async (
   }
 };
 
-export const updateReview = async (id: string) => {
+export const updateBookingReview = async (id: string) => {
   try {
     await API.graphql(
       graphqlOperation(UPDATE_USER_BOOKING_REVIEW_STATUS, {
         id,
       }),
     );
+    console.log(`Updated booking review status for ${id}`);
+  } catch (error) {
+    console.log(error);
+    sentryError(error);
+  }
+};
+
+export const updateMembershipReview = async (id: string) => {
+  try {
+    await API.graphql(
+      graphqlOperation(UPDATE_USER_MEMBERSHIP_REVIEW_STATUS, {
+        id,
+      }),
+    );
+    console.log(`Updated membership review status for ${id}`);
   } catch (error) {
     sentryError(error);
   }
